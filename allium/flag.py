a9e44db4791f18588b35f0f35eab0753debaaa30
Chromosomologist <chromosomologist@gmail.com>
2023-01-19 17:23:31 +0100

    feat: expose member aliases in all aliased classes

--- allium/flag.py
+++ allium/flag.py
@@ -13,12 +13,15 @@ __all__: typing.Sequence[str] = (
 
 T = typing.TypeVar("T")
 EnumT = typing.TypeVar("EnumT", bound="AliasedEnumMeta")
+AEnumT = typing.TypeVar("AEnumT", bound="AliasedEnum")
 
 
 class AliasedEnumDict(enum._EnumDict):
-    _cls_name: str
+
     alias_map: dict[str, alias.Aliased]
+    _cls_name: str
     _generate_next_value: typing.Callable[[str, int, int, list[typing.Any]], int] | None
+    _member_names: str
 
     def __init__(
         self,
@@ -46,6 +49,13 @@ class AliasedEnumDict(enum._EnumDict):
 
     __raw_setitem__ = dict[str, typing.Any].__setitem__
 
+    def get_aliases(self, key: str) -> tuple[str, ...]:
+        alias_obj = self.alias_map[key]
+        if alias_obj:
+            return alias_obj.aliases
+
+        return ()
+
 
 class AliasedEnumMeta(enum.EnumMeta):
     _alias_map_: dict[str, enum.Enum]
@@ -69,11 +79,14 @@ class AliasedEnumMeta(enum.EnumMeta):
         **kwds: typing.Any,
     ) -> EnumT:
         new = super().__new__(metacls, cls, bases, classdict, **kwds)
-        new._alias_map_ = {
-            alias: new._member_map_[key]
-            for key, alias_data in classdict.alias_map.items()
-            for alias in alias_data.aliases
-        }
+
+        new._alias_map_ = {}
+        for name, member in new._member_map_.items():
+            assert isinstance(member, AliasedEnum)
+
+            member._aliases_ = classdict.get_aliases(name)
+            for single_alias in member._aliases_:
+                new._alias_map_[single_alias] = member
 
         return new
 
@@ -101,8 +114,8 @@ class AliasedEnumMeta(enum.EnumMeta):
 
 
 class AliasedEnum(enum.Enum, metaclass=AliasedEnumMeta):
-    """A custom implementation of ::class::`enum.Enum` that supports aliasing.
-    Combine this with ::func::`auto` or ::func::`alias` to easily set aliases
+    """A custom implementation of :class:`enum.Enum` that supports aliasing.
+    Combine this with :func:`auto` or :func:`alias` to easily set aliases
     for enum fields.
 
     Aliased values can be obtained through indexing, just like with names:
@@ -115,10 +128,17 @@ class AliasedEnum(enum.Enum, metaclass=AliasedEnumMeta):
         <MyFlag.x: 1>
     """
 
+    _aliases_: typing.Sequence[str]
+
+    @property
+    def aliases(self) -> typing.Sequence[str]:
+        """The aliases of the enum member, if any."""
+        return self._aliases_
+
 
 class AliasedIntEnum(int, AliasedEnum):
-    """A custom implementation of ::class::`enum.IntEnum` that supports
-    aliasing. Combine this with ::func::`auto` or ::func::`alias` to easily set
+    """A custom implementation of :class:`enum.IntEnum` that supports
+    aliasing. Combine this with :func:`auto` or :func:`alias` to easily set
     aliases for enum fields.
 
     Aliased values can be obtained through indexing, just like with names:
@@ -132,9 +152,12 @@ class AliasedIntEnum(int, AliasedEnum):
     """
 
 
-class AliasedFlag(enum.Flag, metaclass=AliasedEnumMeta):
-    """A custom implementation of ::clas::`enum.Flag` that supports aliasing.
-    Combine this with ::func::`auto` or ::func::`alias` to easily set aliases
+# This complains that Enum.name returns str, whereas Flag.name returns
+# str | None. As this is an "issue" with the stdlib enum implementation, it's
+# safe to ignore.
+class AliasedFlag(enum.Flag, AliasedEnum, metaclass=AliasedEnumMeta):  # pyright: ignore
+    """A custom implementation of :class:`enum.Flag` that supports aliasing.
+    Combine this with :func:`auto` or :func:`alias` to easily set aliases
     for flag fields.
 
     Aliased values can be obtained through indexing, just like with names:
@@ -148,9 +171,9 @@ class AliasedFlag(enum.Flag, metaclass=AliasedEnumMeta):
     """
 
 
-class AliasedIntFlag(enum.IntFlag, metaclass=AliasedEnumMeta):
-    """A custom implementation of ::class::`enum.IntFlag` that supports
-    aliasing. Combine this with ::func::`auto` or ::func::`alias` to easily set
+class AliasedIntFlag(enum.IntFlag, AliasedFlag, metaclass=AliasedEnumMeta):
+    """A custom implementation of :class:`enum.IntFlag` that supports
+    aliasing. Combine this with :func:`auto` or :func:`alias` to easily set
     aliases for flag fields.
 
     Aliased values can be obtained through indexing, just like with names:
@@ -162,3 +185,16 @@ class AliasedIntFlag(enum.IntFlag, metaclass=AliasedEnumMeta):
         >>> MyFlag["Foo"]
         <MyFlag.x: 1>
     """
+
+    @classmethod
+    def _create_pseudo_member_(cls: type[AEnumT], value: typing.Any) -> AEnumT:
+        """Create a composite member iff value contains only members.
+
+        This is the exact same as the standard library
+        :meth:`enum.Flag._create_pseudo_member_`, except it also sets the
+        _aliases_ field.
+        """
+        # Yet another typeshed moment, _create_pseudo_member_ isn't typehinted.
+        member: AEnumT = super()._create_pseudo_member_(value)  # type: ignore
+        member._aliases_ = ()
+        return member
